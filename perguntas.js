42d42c013eb822c83f1144b278d35c44bfe7204e
Matheus Eduardo Alves Ramalho <matheus.alves.ramalho@escola.pr.gov.br>
2025-06-03 18:42:04 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'História',
+    'Quem foi o primeiro presidente do Brasil?',
+    'Deodoro da Fonseca'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o nome do maior oceano da Terra?',
+    'Oceano Pacífico'
+)
+criaCartao(
+    'Artes',
+    'Quem pintou a Mona Lisa?',
+    'Leonardo da Vinci'
+)
+
+criaCartao(
+    'Biologia',
+    'Como se chama o processo das plantas que transforma luz solar em energia?',
+    'Fotossíntese'
+)
